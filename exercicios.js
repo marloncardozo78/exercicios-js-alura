3bddf32733629078ba0034bdb11a58131cf679f0
Marlon Cardozo <marloncardozo@msn.com>
2025-09-30 16:54:51 -0300

    Add files via upload
    Exercícios da primeira aula de JS da Alura.
    Códigos dos exercícios escritos iguais ou bem próximos aos da correção do professor.

---
+++ exercicios.js
@@ -0,0 +1,16 @@
+alert('Boas vindas ao nosso site!');
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('ERRO! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+let seuNome = prompt('Digite seu nome:');
+idade = prompt('Digite sua idade');
+let habilitacao = 18
+if (idade >= habilitacao) {
+    alert('Pode Dirigir');
+}else {
+    alert('Não pode dirigir');    
+}
